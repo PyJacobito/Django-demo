8e77448aad96654c6552b0ecbab4327d72504e2e
PyJacobito <sobel.jak@gmail.com>
2022-10-12 22:08:06 +0200

    Some styling changes

--- django-demo/todo_list/base/views.py
+++ django-demo/todo_list/base/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render, redirect
-from django.views.generic.list import ListView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
-from django.urls import reverse_lazy
-
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.views.generic.list import ListView
 
 from .models import Task
 
